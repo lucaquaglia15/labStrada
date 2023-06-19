0c2b7cdf043fd275711ddee02898b42456d1ae58
lucaquaglia15 <luca.quaglia@cern.ch>
2023-06-19 11:39:28 +0200

    Edidted GUI + added links to manuals

--- gui/gui.py
+++ gui/gui.py
@@ -2,13 +2,68 @@ import PySimpleGUI as sg
 import mysql.connector #to connect to db to send the data
 import psutil
 import os
-import subprocess
+
+def deleteScan(db,cursor,scanType):
+    print("Opening delete scan panel")
+
+    deleteLayout=[
+            [sg.Text('Enter run number',size=(20, 1), font=('Lucida',12,'bold'),justification='left')],
+            [sg.Text('Run number', size=(15, 1)), sg.InputText(key='runNum',default_text='0')],
+            [sg.Button('Delete run',font=('Times New Roman',12)),sg.Button('Back', font=('Times New Roman',12))]
+            ]
+
+    deleteScanWin =sg.Window('Delete run',deleteLayout)
+
+    while True:
+        event, values = deleteScanWin.read()
+
+        if event == 'Delete run' and values['runNum'] != "": #delete run
+            #Delete locally
+
+            #Delete from db
+            
+        elif event == 'Delete run' and values['runNum'] == "": #missing run number -> error
+            errorWindow("Delete run error","Please entrer run number")
+
+#Fetch gas mixtures from db
+def fetchGasMixtures(db,cursor):
+
+    getColumns = ("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='labStrada' AND `TABLE_NAME`='mixtures' ORDER BY ordinal_position")    
+    cursor.execute(getColumns)
+    columns = cursor.fetchall()
+
+    getMixtures = ("SELECT * FROM mixtures")
+    cursor.execute(getMixtures)
+    mixtures = cursor.fetchall()
+    
+    totComposition = [] #list of: [mixname, gas1, gas2.....] for all mixtures in db
+
+    for mixture in mixtures:
+        mixComposition = [] #list of: [mixname, gas1, gas2.....] for a single mixture, updated for every cycle of the loop
+        
+        for i, element in enumerate(mixture):
+            if element != 0:
+                if columns[i][0] != 'name':
+                    mixComposition.append(str(element)+"% "+columns[i][0])
+                else:
+                    mixComposition.append(str(element))
+    
+        totComposition.append(mixComposition[:])
+        mixComposition = []
+
+    return totComposition
+
+def deleteMixture(db,cursor,mixName):
+    print(mixName)
+    delete = ("DELETE FROM labStrada.mixtures WHERE name = (%s)")
+    name = (mixName,)
+    cursor.execute(delete,name)
 
 #New mixture window
 def newMixture(db,cursor):
     print("Opening new mixture panel")
 
-    sum = 0 #sum of percentages, sanity check
+    sum = 0 #sum of percentages, check of mixture sanity
 
     mixLayout=[
             [sg.Text('Enter new mixture',size=(20, 1), font=('Lucida',12,'bold'),justification='left')],
@@ -41,13 +96,23 @@ def newMixture(db,cursor):
                 if key != "name" and float(values[key]) > 0:
                     sum = sum + float(values[key])
 
+            #Check if mixture with same name but different conentrations exists
+            checkForMixture = ("SELECT EXISTS(SELECT * FROM mixtures WHERE name=(%s))")
+            name = (values["name"],)
+
+            cursor.execute(checkForMixture,name)
+            isMixture = cursor.fetchall()
+
+            if isMixture[0][0] == 1: #mixture with this name already in db
+                errorWindow("Gas mixture error","Mixture with same name but different concentration already exists in database")
+            
             #Check if sum of concentrations == 100
-            if sum != 100: #sum of gases is not 100 -> error
+            elif sum != 100: #sum of gases is not 100 -> error
                 errorWindow("Gas mixture error","Sum of percentages is not 100")
 
             elif values["name"] == "":
                 errorWindow("Gas mixture error","Mixture has no name")
-            
+                    
             else: #add new mixture to db
                 mixture = [values["name"],values["HFOze"],values["HFOyf"],values["R134a"],values["i-C4H10"],values["SF6"],values["CO2"],values["N2"],values["O2"],values["Ar"]] 
                 sendMixture = ("INSERT INTO `mixtures` (name,HFOze,HFOyf,R134a,iC4H10,SF6,CO2,N2,O2,Ar) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
@@ -111,32 +176,11 @@ def main():
             password="pcald32",
             database="labStrada"
         )
-
+    
     mycursor = mydb.cursor()
 
-    getColumns = ("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='labStrada' AND `TABLE_NAME`='mixtures' ORDER BY ordinal_position")    
-    mycursor.execute(getColumns)
-    columns = mycursor.fetchall()
-
-    getMixtures = ("SELECT * FROM mixtures")
-    mycursor.execute(getMixtures)
-    mixtures = mycursor.fetchall()
+    totComposition = fetchGasMixtures(mydb,mycursor)
     
-    totComposition = [] #list of: [mixname, gas1, gas2.....] for all mixtures in db
-
-    for mixture in mixtures:
-        mixComposition = [] #list of: [mixname, gas1, gas2.....] for a single mixture, updated for every cycle of the loop
-        
-        for i, element in enumerate(mixture):
-            if element != 0:
-                if columns[i][0] != 'name':
-                    mixComposition.append(str(element)+"% "+columns[i][0])
-                else:
-                    mixComposition.append(str(element))
-    
-        totComposition.append(mixComposition[:])
-        mixComposition = []
-
     #check status of running scripts
     hvMonit = True #If True the hv monitoring script is running
     gasKill = True #If True the gas kill script is running
@@ -184,8 +228,10 @@ def main():
             [sg.Text('Choose run type',size=(20, 1), font=('Lucida',12,'bold'),justification='left')],
             [sg.Combo(['Current scan','Efficiency scan','Noise scan', 'Stability scan', 'Resistivity measurement'],default_value='Current scan',key='scanType')],
             [sg.Text('Choose Mixture ',size=(30, 1), font=('Lucida',12,'bold'),justification='left')],
-            [sg.Listbox(values=totComposition, select_mode='extended', key='mixture', size=(100, 6))],
-            [sg.Text('Or enter new gas mixture',size=(30,1),font=('Lucida',12,'bold'),justification='left'),sg.Button('New mixture', font=('Times New Roman',12))],
+            [sg.Listbox(values=totComposition, select_mode='single', key='mixture', size=(100, 6),enable_events=True)],
+            [sg.Text('Enter new gas mixture',size=(30,1),font=('Lucida',12,'bold'),justification='left'),sg.Button('New mixture', font=('Times New Roman',12))],
+            [sg.Text('Delete gas mixture',size=(30,1),font=('Lucida',12,'bold'),justification='left'),sg.Button('Delete mixture', font=('Times New Roman',12),disabled=True,key='deleteMixture')],
+            [sg.Text('Delete scan',size=(30,1),font=('Lucida',12,'bold'),justification='left'),sg.Button('Delete scan', font=('Times New Roman',12),key='deleteScan')],
             [sg.Button('Start scan', font=('Times New Roman',12)),sg.Button('Abort scan', font=('Times New Roman',12))]
             ]
 
@@ -193,36 +239,71 @@ def main():
 
     while True:
         event, values = win.read()
-        
+
         #Close program
         if event == "Abort scan" or event == sg.WIN_CLOSED:
             break
 
+        #If mixture is selected once from list and it is re-clicked
+        #-> Remove selection and disable delete mixture button
+        if event == 'mixture':
+            metadata = win['mixture'].metadata
+            selections = values['mixture']
+            if metadata == selections:
+                win['mixture'].update(set_to_index=[])
+                win['mixture'].metadata = []
+                values['mixture'] = ""
+                win['deleteMixture'].update(disabled=True)
+            else:
+                win['mixture'].metadata = values['mixture']
+
+        #Enable delete mixture buttton if a gas mixture is selected from the list       
+        if str(values['mixture']) != "":    
+            win['deleteMixture'].update(disabled=False)
+        
         #Enter new mixture in db
         if event == "New mixture":
             newMixture(mydb,mycursor)
+            mydb.cmd_refresh(1)
+            updatedMixtures = fetchGasMixtures(mydb,mycursor)
+            win['mixture'].update(updatedMixtures)
 
+        #Delete gas mixture new mixture in db
+        elif event == "Delete mixture":
+            deleteMixture(mydb,mycursor,values['mixture'][0][0])
+            mydb.cmd_refresh(1)
+            updatedMixtures = fetchGasMixtures(mydb,mycursor)
+            win['mixture'].update(updatedMixtures)
+
+        #Delete scan data both locally and from db
+        if event == "Delete scan":
+            deleteScan(mydb,mycursor,values[''])
+        
         #Start scan
         if event == "Start scan":
-            if values['scanType'] == 'Current scan':
+            if values['scanType'] == 'Current scan': #General current scan
                 print('You selected current scan')
                 #print(values['mixture'][0][0])
                 os.system("python3 /home/pcald32/labStrada/DAQ/currentScan/hvScan.py " + str(values['mixture'][0][0] + " currentScan"))
 
-            elif values['scanType'] == 'Efficiency scan':
+            elif values['scanType'] == 'Efficiency scan': #efficiency scan (muon trigger)
                 print('You selected efficiency scan')
                 os.system("python3 /home/pcald32/labStrada/DAQ/efficiencyScan/effScan.py " + str(values['mixture'][0][0] + " efficiencyScan"))
 
-            elif values['scanType'] == 'Noise scan':
+            elif values['scanType'] == 'Noise scan': #noise scan (random trigger)
                 print('You selected noise scan')
                 os.system("python3 /home/pcald32/labStrada/DAQ/efficiencyScan/effScan.py " + str(values['mixture'][0][0] + " noiseScan"))
 
-            elif values['scanType'] == 'Stability scan':
+            elif values['scanType'] == 'Stability scan': #stability scan
                 print('You selected Stability scan')
 
-            elif values['scanType'] == 'Resistivity measurement':
+            elif values['scanType'] == 'Resistivity measurement': #res scan
                 print('You selected resistivity measurement')
-                os.system("python3 /home/pcald32/labStrada/DAQ/currentScan/hvScan.py " + str(values['mixture'][0][0] + " argonScan"))
+                
+                if str(values['mixture'][0][0]) != 'argon': #res scan but not Ar as the gas
+                    errorWindow("Gas mixture error","You selected resistivity scan but not Ar as the gas")
+                else: #res scan and Ar as the gas
+                    os.system("python3 /home/pcald32/labStrada/DAQ/currentScan/hvScan.py " + str(values['mixture'][0][0] + " argonScan"))
             
             break
 
